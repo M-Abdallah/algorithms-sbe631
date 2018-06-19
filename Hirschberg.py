66efa4bc92a3f4f42211e2b6df7ece8eed67dce5
mohamedAbdallah <mohamed.m.i.abdallah@gmail.com>
2018-06-19 02:05:42 +0200

    Adding comments to the Hirschberg

--- Hirschberg.py
+++ Hirschberg.py
@@ -16,6 +16,7 @@ scoring = np.array([[3,-3,-3,-3],
 
 
 def scoreCalc( seq1, seq2 ):
+    # This calculate the of the scoreGrid for those sequences seq1 and seq2
     scoreGrid = np.zeros((seq1.size+1, seq2.size+1), dtype=np.int16)
     # Fill the score grid(matching grid)
     for i in range(seq1.size+1):
@@ -32,6 +33,7 @@ def scoreCalc( seq1, seq2 ):
     return scoreGrid
 
 def NWscore(seq1, seq2):
+    # This calculate the last row of the of the scoreGrid for those sequences seq1 and seq2
     scoreGrid = scoreCalc( seq1, seq2 )
     return scoreGrid[seq1.size, :]
 
@@ -47,27 +49,25 @@ def getAlignedPair(seq1, seq2, i, j):
     return (n1, n2)
 
 
-def __traceback__(seqX, seqY):
-    print(seqX, seqY)
+def traceback(seqX, seqY):
     # Translate the score(matching) grid to matched pairs or gaps
     alignedPair = []
     i = seqX.size
     j = seqY.size
     # scoreGrid = scoreCalc( seqX, seqY )
     if j == 0:
-        print('j=0')
+        # This is search for direct mismatch and add gaps for the second sequence
         while i > 0:
             alignedPair.append(getAlignedPair(seqX, seqY, i, 0))
             i -= 1
     elif i == 0:
-        print('i=0')
+        # This is search for direct mismatch and add gaps for the first sequence
         while j > 0:
             alignedPair.append( getAlignedPair(seqX, seqY,0, j))
             j -= 1
     elif i == 1 or j == 1:
-        print('j=1 and i=1')
+        # This is just normal Needleman-Wunsch Algorithm 
         scoreGrid = scoreCalc( seqX, seqY )
-        print(scoreGrid)
         while i > 0 or j > 0:
             if i > 0 and j > 0 and scoreGrid[i-1, j-1] + getScore( seqX,seqY, i, j) == scoreGrid[i, j]:
                 alignedPair.append(getAlignedPair( seqX,seqY, i, j))
@@ -80,15 +80,16 @@ def __traceback__(seqX, seqY):
                 alignedPair.append(getAlignedPair(seqX, seqY, 0, j))
                 j -= 1
     else:
-        print('Hirscberg')
+        # Here is were all the magic happens, and by magic I mean Divide&Conquer happens
+        # Where the First sequence is divided
         iMid = int(i/2)
         scoreL = NWscore( seqX[0:iMid], seqY )
         scoreR = NWscore( seqX[iMid:i][::-1], seqY[::-1] )
         sum = np.add(scoreL, scoreR[::-1] )
         
         jMid = np.argmax( sum )
-        alignedPair+= __traceback__ ( seqX[0:iMid], seqY[ 0: jMid ] )
-        alignedPair+= __traceback__ ( seqX[iMid:i], seqY[ jMid: j ] )  
+        alignedPair+= traceback ( seqX[0:iMid], seqY[ 0: jMid ] )
+        alignedPair+= traceback ( seqX[iMid:i], seqY[ jMid: j ] )  
     
     print(alignedPair)
     return alignedPair
@@ -99,7 +100,7 @@ if __name__ == "__main__":
     # array2 = ['G', 'G', 'T', 'A', 'C', 'G', 'T']
     seq1 = np.array([A,G,T,A,C,G,C,A], dtype=np.int16)
     seq2 = np.array([T,A,T,G,C], dtype=np.int16)
-    aligned = __traceback__( seq1, seq2 )
+    aligned = traceback( seq1, seq2 )
     # print(aligned)
     # aligned.reverse()
     # print(aligned)
